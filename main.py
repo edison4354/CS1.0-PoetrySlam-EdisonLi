39aa2907f547a6f6efbe0bdd4aa64e01ccd3bd89
Edison <edison4354@gmail.com>
2020-09-30 21:26:18 -0700

    Created Lines Printed Backward Function
    This Function prints the lines of the poem in reverse. Include the original line number at the beginning of each line.

--- main.py
+++ main.py
@@ -4,5 +4,10 @@ def get_file_lines(filename):
     read_file = open(filename, 'r')
     return read_file.readlines()
 
-print(get_file_lines('poem.txt'))
+def lines_printed_backwards(lines_list):
+    lines_list_reverse = lines_list[::-1]
+    for string in lines_list_reverse:
+        line_number = lines_list.index(string) + 1
+        print(line_number, string)
 
+lines_printed_backwards(get_file_lines('poem.txt'))
