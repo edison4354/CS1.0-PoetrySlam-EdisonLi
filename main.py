c17e8be34657b8a8d6fc09b14064a1467474452d
Edison <edison4354@gmail.com>
2020-09-30 22:25:03 -0700

    Created the lines_printed_random function

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
+import random
 
 # Reads the poem file then return a list of strings containing the lines of the file
 def get_file_lines(filename):
     read_file = open(filename, 'r')
     return read_file.readlines()
 
+# Reverses the list of strings/lines from the poem then prints them line by line including the original line number 
 def lines_printed_backwards(lines_list):
-    lines_list_reverse = lines_list[::-1]
+    lines_list_reverse = lines_list[::-1] # reverses the list of lines and assigned it to another varible
     for string in lines_list_reverse:
-        line_number = lines_list.index(string) + 1
+        line_number = lines_list.index(string) + 1 # assigned a varible the index of the string containing the line
         print(line_number, string)
 
-lines_printed_backwards(get_file_lines('poem.txt'))
+def lines_printed_random(lines_list):
+    for i in range(len(lines_list)):
+        random_index = random.randint(0, len(lines_list))
+        print(lines_list[random_index - 1])
+
+# def lines_printed_random(lines_list):
+#     random.shuffle(lines_list)
+#     for string in lines_list:
+#         print(string)
+
+
+# def lines_printed_custom(lines_list):
+#     pass
+
+def main():
+    lines_printed_backwards(get_file_lines('poem.txt'))
+    lines_printed_random(get_file_lines('poem.txt'))
+    # lines_printed_custom(get_file_lines('poem.txt'))
+
+if __name__ == "__main__":
+    main()
